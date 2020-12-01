b1ac72a5c67d0df695cebf8469236af47bd0f1ac
antoinegautreau <agautre2@unb.ca>
2020-12-01 18:15:41 -0400

    basic working login

---
+++ WebGameSubscription/website.js
@@ -0,0 +1,35 @@
+
+//Is called when you hit the "submit" button on the Login page
+function validateForm()
+{
+    var username = document.forms["login"]["uname"].value;
+    var password = document.forms["login"]["pswd"].value;
+
+    //if valid login info brings you to next page
+    if(username == "a" && password == "a")
+    {
+        //I think to do the whole redirect thing you need 
+        redirect();    
+    }
+    else
+    {
+        alert("This isn't an account fool! Ya fool!");
+    }
+
+    //need to put false here otherwise the page won't get redirected
+    return false;
+}
+
+
+function redirect()
+{
+    /*--------------------------------------------------------------------------------
+      Basically by default when you click on the submit button the form gets submitted
+      but this cancels any ongoing HTTP requests from going through so you can 
+      by pass this by just returning false.
+    ---------------------------------------------------------------------------------*/
+
+    window.location.href = 'gameList.html';
+    return false;
+}
+        

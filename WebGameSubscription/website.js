d6fd709b911e795972adc58d927877539b2d40a1
thomasgautreau3 <tgautrea@unb.ca>
2020-12-02 11:19:46 -0400

    New game pages

---
+++ WebGameSubscription/website.js
@@ -0,0 +1,23 @@
+
+ 
+
+// // var w = document.getElementById('write');
+// // w.onclick = writeToFile;
+// const fs = require('fs');
+
+// function writeToFile(){
+//         // Requiring fs module in which 
+//     // writeFile function is defined. 
+     
+    
+//     // Data which will write in a file. 
+//     let data = "Learning how to write in a file."
+//     console.log("It did this!");
+//     // Write data in 'Output.txt' . 
+//      fs.writeFile('Output.txt', data, (err) => { 
+        
+//         // In case of a error throw err. 
+//         if (err) throw err; 
+//     }) 
+//     //return false;
+// }

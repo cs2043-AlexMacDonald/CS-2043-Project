f687e0c632d1c90729e3a4f0a997fc75450d3065
thomasgautreau3 <tgautrea@unb.ca>
2020-12-07 10:31:45 -0400

    Bootstrap

--- WebGameSubscription/app.js
+++ WebGameSubscription/app.js
@@ -1,1 +1,15 @@
 //File for web server & Database
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "127.0.0.1",
+    port: "3306",
+    user: "root",
+    password: "254moyw#" //MAKE SURE TO NOT ACTUALLY PUT YOUR INFO WHEN PUSHING CODE TO GITHUB
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
